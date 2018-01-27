443926ff380aadd00b61912d393aab96d58400f2
EemeliSaari <saari.eemeli@gmail.com>
2018-01-27 14:47:35 +0200

    Added more feature extractions

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -5,4 +5,12 @@ def straight_forward(raw):
     """Converts matrixes to vector features"""
     return np.array([x.ravel() for x in raw])
 
-#TODO add more feature extraction
+
+def mean_over_time(raw):
+    """Takes a mean value for each data matrix"""
+    return np.array([np.mean(x, axis=1) for x in raw])
+
+
+def mean_over_freq(raw):
+    """Takes a mean value for each data matrix"""
+    return np.array([np.mean(x, axis=0) for x in raw])
