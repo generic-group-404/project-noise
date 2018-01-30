7ffeedc7013471d2cd4cc8f1fcc169fdd32f2710
EemeliSaari <saari.eemeli@gmail.com>
2018-01-30 19:53:53 +0200

    Added submission
    Added submission option and debugging for more straight forward testing.

--- eval_models.py
+++ eval_models.py
@@ -2,49 +2,81 @@
 
 # Evaluate different modesl
 
+import warnings
+from time import time
+
 import numpy as np
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 
 import src.feature_extraction as features
-from models.svm_model import SVMModel
 from models.logistic_regression_model import LR_model
+from models.svm_model import SVMModel
 from src.label_mapper import Mapper
+from src.save_local_files import save_result
 
 
-#TODO convert to a class
-def evaluate(data_path="data/"):
+def evaluate(method, models, data_path="data/", debug=True, submission=False):
 
     # Mapper inherits the sklearn LabelEncoder methods
     mapper = Mapper('{:s}y_train.csv'.format(data_path))
 
-    x_raw = np.load('{:s}X_train.npy'.format(data_path))
-    y = mapper.fitted
+    X_train = np.load('{:s}X_train.npy'.format(data_path))
+    y_train = mapper.fitted
 
-    # Use only the 20% of the data set to speed up the training
-    X_train, X_test, y_train, y_test = train_test_split(x_raw, y, test_size=.02, train_size=.1)
-    
-    # FULL CASE
-    # X_train, X_test, y_train, y_test = train_test_split(x_raw, y, test_size=.2, train_size=.8)
+    if not submission:
+        if debug:
+            # Use only the 20% of the data set to speed up the training
+            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.02, train_size=.1)
+        else:
+            # Full training case
+            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.2, train_size=.8)
+    else:
+        X_test = np.load('{:s}X_test.npy'.format(data_path))
+        # Since there seems to be warning in the sklearn.preprocessing files we filter it.
+        warnings.simplefilter("ignore")
 
     # Extract the features after the split to avoid information loss
-    X_train = features.straight_forward(X_train)
-    X_test = features.straight_forward(X_test)
-
-    # Add/remove tested models here
-    models = [SVMModel(), SVMModel('linear'), LR_model()]
+    X_train = method(X_train)
+    X_test = method(X_test)
 
     # Iterate through models
     results = dict()
     for model in models:
-        model.fit(X_train, y_train)
-        results[str(model)] = model.score(X_test, y_test)
+        meta = dict()
 
-    print(results)
+        # Train the model
+        training_time = time()
+        model.fit(X_train, y_train)
+        meta['tr_time'] = time() - training_time
+
+        # Predict the labels
+        predicting_time = time()
+        predictions = model.predict(X_test)
+        meta['pre_time'] = time() - predicting_time
+
+        if submission:
+            results = mapper.inverse_transform(predictions)
+            save_result(str(model), results)
+        else:
+            score = accuracy_score(y_test, predictions)
+            meta['score'] = score
+            if debug:
+                print('{:s}: {:.6f}'.format(str(model), score))
+            results[str(model)] = meta
+
+    return results
 
 
 if __name__ == '__main__':
 
-    evaluate()
+    # Add/Remove tested models here.
+    models = [SVMModel(), SVMModel('linear'), LR_model()]
+
+    # Run the evaluation function
+    evaluate(features.mean_over_time, models, submission=False, debug=True)
+
     # add different path for data files:
     # example usage:
-    # evaluate("../MyFolder/data/")
+    # evaluate(.. data_path = "../MyFolder/data/" ..)
+
