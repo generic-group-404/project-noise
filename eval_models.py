c490faf9cc884e8cace78184b3542786f1d8bbb2
EemeliSaari <saari.eemeli@gmail.com>
2018-01-31 13:32:08 +0200

    Adjusted the evaluate function

--- eval_models.py
+++ eval_models.py
@@ -8,10 +8,12 @@ from time import time
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 import src.feature_extraction as features
 from models.logistic_regression_model import LR_model
 from models.svm_model import SVMModel
+from src.cross_validation import cross_validation_data
 from src.label_mapper import Mapper
 from src.save_local_files import save_result
 
@@ -21,18 +23,21 @@ def evaluate(method, models, data_path="data/", debug=True, submission=False):
     # Mapper inherits the sklearn LabelEncoder methods
     mapper = Mapper('{:s}y_train.csv'.format(data_path))
 
-    X_train = np.load('{:s}X_train.npy'.format(data_path))
-    y_train = mapper.fitted
-
     if not submission:
+        X_train, X_test, y_train, y_test = cross_validation_data(data_path)
+
         if debug:
             # Use only the 20% of the data set to speed up the training
-            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.02, train_size=.1)
-        else:
-            # Full training case
-            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.2, train_size=.8)
+            X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=.02, train_size=.2)
+
+        y_test = mapper.transform(y_test)
+        y_train = mapper.transform(y_train)
     else:
+        X_train = np.load('{:s}X_train.npy'.format(data_path))
         X_test = np.load('{:s}X_test.npy'.format(data_path))
+
+        y_train = mapper.fitted
+
         # Since there seems to be warning in the sklearn.preprocessing files we filter it.
         warnings.simplefilter("ignore")
 
@@ -56,8 +61,8 @@ def evaluate(method, models, data_path="data/", debug=True, submission=False):
         meta['pre_time'] = time() - predicting_time
 
         if submission:
-            results = mapper.inverse_transform(predictions)
-            save_result(str(model), results)
+            result = mapper.inverse_transform(predictions)
+            save_result(str(model), result)
         else:
             score = accuracy_score(y_test, predictions)
             meta['score'] = score
@@ -79,4 +84,3 @@ if __name__ == '__main__':
     # add different path for data files:
     # example usage:
     # evaluate(.. data_path = "../MyFolder/data/" ..)
-
