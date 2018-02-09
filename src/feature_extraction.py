7e74df30584909d9ae544e9b58b07784f3e03fc1
Eemeli Saari <saari.eemeli@gmail.com>
2018-02-09 08:53:22 +0200

    Add split and ravel splitted functions

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -14,3 +14,19 @@ def mean_over_time(raw):
 def mean_over_freq(raw):
     """Takes a mean value for each data matrix"""
     return np.array([np.mean(x, axis=0) for x in raw])
+
+
+def split_extract_data(method, n, axis, data):
+    """Splits and extracts features from the data according to the n"""
+    return np.array([method(np.split(d, n, axis=axis)) for d in data])
+
+
+def ravel_splitted_data(X, y):
+    """Ravels the splitted data"""
+    r_X = []
+    r_y = []
+    for i, row in enumerate(X):
+        for entry in row:
+            r_X.append(entry)
+            r_y.append(y[i])
+    return np.array(r_X), np.array(r_y)
