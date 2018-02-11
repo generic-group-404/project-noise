bca70ec1c6c220097577790dbe381706003a807a
Eemeli Saari <saari.eemeli@gmail.com>
2018-02-11 22:40:22 +0200

    added cross_validation data path

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 
-def cross_validation_data(data_path='data/'):
+def cross_validation_data(data_path='data/', cross_validation_path='data/'):
     """Splits the data according to the crossvalidation file"""
 
     X_raw = np.load('{:s}X_train.npy'.format(data_path))
 
     X = {'train':[], 'test':[]}
     y = {'train':[], 'test':[]}
-    with open('{:s}crossvalidation_train.csv'.format(data_path), 'r') as f:
+    with open('{:s}crossvalidation_train.csv'.format(cross_validation_path), 'r') as f:
 
         for row in zip(f.readlines()[1:], X_raw):
             parts = row[0].rstrip().split(",")
